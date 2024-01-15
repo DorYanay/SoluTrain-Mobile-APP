29707362bd21d37ffcfe04032477269c2e8ef330
Omer Priel <omer.priel2@gmail.com>
2024-01-15 14:30:48 +0200

    create base (config, logger, db connection) for backend server.

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,33 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator
+
 from fastapi import FastAPI
+
+from src.config import init_config
+from src.logger import get_logger, init_loggers
+from src.models import close_db, init_db
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
+    init_config()
+    init_loggers()
+
+    get_logger().info("The server started.")
+
+    init_db()
+
+    yield None
+
+    close_db()
+
+    get_logger().info("The server closed.")
+
 
 app = FastAPI(
     title="SoluTrain",
     description="SoluTrain API",
+    lifespan=lifespan,
 )
 
 
