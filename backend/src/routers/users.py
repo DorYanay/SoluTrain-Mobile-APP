8d72b26423df9c3fe17873db7672404b13581007
Omer Priel <omer.priel2@gmail.com>
2024-01-16 16:49:34 +0200

    create signup route

--- backend/src/routers/users.py
+++ backend/src/routers/users.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from uuid import UUID
 
 import psycopg
@@ -5,7 +7,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 
 from src.models import db_dependency
-from src.models.users import Gender, create_user, get_user_by_email, get_user_by_id
+from src.models.users import Gender, User, create_user, get_user_by_email, get_user_by_id
 
 router = APIRouter()
 
@@ -14,8 +16,46 @@ class UserSchema(BaseModel):
     user_id: str
     name: str
     email: str
+    phone: str
     gender: Gender
     description: str
+    is_trainer: bool
+    is_coach: bool
+
+    @staticmethod
+    def from_model(user: User) -> UserSchema:
+        return UserSchema(
+            user_id=str(user.user_id),
+            name=user.name,
+            email=user.email,
+            phone=user.phone,
+            gender=user.gender,
+            description=user.description,
+            is_trainer=user.is_trainer,
+            is_coach=user.is_coach,
+        )
+
+
+@router.post("/signup")
+async def route_signup(
+    name: str,
+    email: str,
+    password: str,
+    phone: str,
+    gender: Gender,
+    is_trainer: bool,
+    is_coach: bool,
+    db: psycopg.Connection = Depends(db_dependency),
+) -> UserSchema:
+    if get_user_by_email(db, email) is not None:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists with this email")
+
+    # TODO: Hash password
+    password_hash = "#" + password
+
+    user = create_user(db, name, email, password_hash, phone, gender, is_trainer, is_coach)
+
+    return UserSchema.from_model(user)
 
 
 @router.get("/{user_id}")
@@ -25,26 +65,4 @@ async def route_get_user(user_id: UUID, db: psycopg.Connection = Depends(db_depe
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
-    return UserSchema(
-        user_id=str(user.user_id),
-        name=user.name,
-        email=user.email,
-        gender=user.gender,
-        description=user.description,
-    )
-
-
-@router.post("/create")
-async def route_create_user(name: str, email: str, gender: Gender, db: psycopg.Connection = Depends(db_dependency)) -> UserSchema:
-    if get_user_by_email(db, email) is not None:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="User already exists with this email")
-
-    user = create_user(db, name, email, gender)
-
-    return UserSchema(
-        user_id=str(user.user_id),
-        name=user.name,
-        email=user.email,
-        gender=user.gender,
-        description=user.description,
-    )
+    return UserSchema.from_model(user)
