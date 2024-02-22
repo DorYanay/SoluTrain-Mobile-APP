c8551442d656c620fd12d65e04350584c02d115c
Omer Priel <omer.priel2@gmail.com>
2024-02-22 12:09:38 +0200

    add API.guestGet, API.get and API.getURL

--- mobile/lib/api.dart
+++ mobile/lib/api.dart
@@ -76,6 +76,50 @@ class API {
     return Response(data, 200, '', '');
   }
 
+  static Future<Response> guestGet(String endpoint,
+      {Map<String, dynamic>? params}) async {
+    if (kDebugMode) {
+      print("API GET send Request: $endpoint");
+    }
+
+    final response = await http.get(
+        Uri(
+            scheme: Config.apiIsHttps ? 'https' : 'http',
+            host: Config.apiHost,
+            port: Config.apiPort,
+            path: endpoint,
+            queryParameters: params));
+
+    if (kDebugMode) {
+      print("API GET get Response: $endpoint - ${response.statusCode}");
+    }
+
+    if (response.statusCode != 200) {
+      String errorMessage = '';
+
+      try {
+        dynamic errorObject = jsonDecode(response.body);
+
+        dynamic detail = errorObject['detail'];
+
+        if (detail is String) {
+          errorMessage = detail;
+        }
+      } on FormatException {
+        errorMessage = '';
+      }
+
+      if (kDebugMode) {
+        print("API POST ERROR Message: $endpoint - $errorMessage");
+        print("API POST ERROR Body: $endpoint - ${response.body}");
+      }
+
+      return Response(null, response.statusCode, response.body, errorMessage);
+    }
+
+    return Response(response.body, 200, '', '');
+  }
+
   static Future<Response> post(BuildContext context, String endpoint,
       {Map<String, dynamic>? params, String? filePath}) async {
     params ??= <String, dynamic>{};
@@ -84,6 +128,34 @@ class API {
         Provider.of<AppModel>(context, listen: false).authToken;
 
     return API.guestPost(endpoint, params: params, filePath: filePath);
+  }
+
+  static Future<Response> get(BuildContext context, String endpoint,
+      {Map<String, dynamic>? params}) async {
+    params ??= <String, dynamic>{};
+
+    params['auth_token'] =
+        Provider.of<AppModel>(context, listen: false).authToken;
+
+    return API.guestGet(endpoint, params: params);
+  }
+
+  static String getURL(BuildContext context, String endpoint,
+      {Map<String, dynamic>? params}) {
+    params ??= <String, dynamic>{};
+
+    params['auth_token'] =
+        Provider.of<AppModel>(context, listen: false).authToken;
+
+    Uri uri = Uri(
+        scheme: Config.apiIsHttps ? 'https' : 'http',
+        host: Config.apiHost,
+        port: Config.apiPort,
+        path: endpoint,
+        queryParameters: params
+    );
+
+    return uri.toString();
   }
 
   API._();
