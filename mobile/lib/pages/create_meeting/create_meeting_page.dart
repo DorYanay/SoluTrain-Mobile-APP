b7453551e6d6431aa308e9afe6bd6f34287ef370
Omer Priel <omer.priel2@gmail.com>
2024-02-25 18:15:10 +0200

    wip

--- mobile/lib/pages/create_meeting/create_meeting_page.dart
+++ mobile/lib/pages/create_meeting/create_meeting_page.dart
@@ -4,6 +4,8 @@ import 'package:mobile/schemas.dart';
 import 'package:provider/provider.dart';
 
 class CreateMeetingPage extends StatefulWidget {
+  final meetingOpeningDayLimit = 60;
+
   final String groupId;
 
   const CreateMeetingPage(this.groupId, {super.key});
@@ -13,22 +15,20 @@ class CreateMeetingPage extends StatefulWidget {
 }
 
 class _CreateMeetingPageState extends State<CreateMeetingPage> {
-  late TextEditingController meetingNameController;
-  AreaSchema? selectedRegion;
-  late TextEditingController descriptionController;
+  late TextEditingController maxMembersController= TextEditingController();
 
-  @override
-  void initState() {
-    super.initState();
-    meetingNameController = TextEditingController();
-    descriptionController = TextEditingController();
+  void datePicker() {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(Duration(days: widget.meetingOpeningDayLimit)),
+
+    )
   }
 
-  @override
-  void dispose() {
-    meetingNameController.dispose();
-    descriptionController.dispose();
-    super.dispose();
+  void createMeetingOnPressed() {
+
   }
 
   @override
@@ -43,69 +43,26 @@ class _CreateMeetingPageState extends State<CreateMeetingPage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             TextFormField(
-              controller: meetingNameController,
+              controller: maxMembersController,
               decoration: const InputDecoration(
-                labelText: 'Meeting Name',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            DropdownButtonFormField<AreaSchema?>(
-              value: selectedRegion,
-              onChanged: (AreaSchema? newValue) {
-                setState(() {
-                  selectedRegion = newValue;
-                });
-              },
-              items: Provider.of<AppModel>(context)
-                  .areas
-                  .map<DropdownMenuItem<AreaSchema?>>((AreaSchema area) {
-                return DropdownMenuItem<AreaSchema?>(
-                  value: area,
-                  child: Text(area.name), // Display the option text
-                );
-              }).toList(),
-              decoration: const InputDecoration(
-                labelText: 'Region',
+                labelText: 'Max Members',
                 border: OutlineInputBorder(),
               ),
             ),
             const SizedBox(height: 20),
             TextFormField(
-              controller: descriptionController,
+              controller: maxMembersController,
               decoration: const InputDecoration(
-                labelText: 'Description',
+                labelText: 'Date',
+                filled: true,
+                prefixIcon: Icon(Icons.calendar_today),
                 border: OutlineInputBorder(),
               ),
-              maxLines: 3,
+              readOnly: true,
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Validate input and save the Meeting
-                if (meetingNameController.text.isEmpty ||
-                    descriptionController.text.isEmpty) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Please fill all fields'),
-                    ),
-                  );
-                  return;
-                }
-
-                // Save the Meeting or perform any necessary action
-                // For example, you can send the Meeting data to the server
-
-                // Clear text fields after saving
-                meetingNameController.clear();
-                descriptionController.clear();
-
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text('Meeting created successfully!'),
-                  ),
-                );
-              },
+              onPressed: createMeetingOnPressed,
               child: const Text('Create Meeting'),
             ),
           ],
