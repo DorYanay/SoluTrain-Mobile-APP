48777b192d52155f8c9bb484d873c550e3a027a6
Omer Priel <omer.priel2@gmail.com>
2024-03-04 14:17:55 +0200

    my_meetings_page.dart

--- mobile/lib/pages/my_meetings/my_meetings_page.dart
+++ mobile/lib/pages/my_meetings/my_meetings_page.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
-class Meeting {
-  final String date;
-  final String day;
-  final String hour;
-  final String description;
-
-  Meeting({
-    required this.date,
-    required this.day,
-    required this.hour,
-    required this.description,
-  });
-}
+import 'package:mobile/app_model.dart';
+import 'package:mobile/schemas.dart';
+import 'package:mobile/api.dart';
 
 class MyMeetingsPage extends StatefulWidget {
   const MyMeetingsPage({super.key});
@@ -22,81 +14,47 @@ class MyMeetingsPage extends StatefulWidget {
 }
 
 class _MyMeetingsPageState extends State<MyMeetingsPage> {
-  final List<Meeting> meetings = [
-    Meeting(
-        date: '2024-02-10',
-        day: 'Monday',
-        hour: '10:00 AM',
-        description: 'Team Meeting'),
-    Meeting(
-        date: '2024-02-12',
-        day: 'Wednesday',
-        hour: '2:00 PM',
-        description: 'Client Presentation'),
-    Meeting(
-        date: '2024-02-14',
-        day: 'Friday',
-        hour: '9:00 AM',
-        description: 'Project Review'),
-    Meeting(
-        date: '2024-02-11',
-        day: 'Tuesday',
-        hour: '11:30 AM',
-        description: 'Product Demo'),
-    Meeting(
-        date: '2024-02-13',
-        day: 'Thursday',
-        hour: '3:00 PM',
-        description: 'Brainstorming Session'),
-    Meeting(
-        date: '2024-02-15',
-        day: 'Saturday',
-        hour: '10:00 AM',
-        description: 'Training Workshop'),
-    Meeting(
-        date: '2024-02-16',
-        day: 'Sunday',
-        hour: '1:00 PM',
-        description: 'Weekly Review Meeting'),
-    Meeting(
-        date: '2024-02-17',
-        day: 'Monday',
-        hour: '9:00 AM',
-        description: 'Project Kickoff'),
-    Meeting(
-        date: '2024-02-19',
-        day: 'Wednesday',
-        hour: '4:30 PM',
-        description: 'Client Feedback Session'),
-    Meeting(
-        date: '2024-02-20',
-        day: 'Thursday',
-        hour: '12:00 PM',
-        description: 'Lunch Meeting'),
-    Meeting(
-        date: '2024-02-21',
-        day: 'Friday',
-        hour: '3:30 PM',
-        description: 'Team Building Activity'),
-    Meeting(
-        date: '2024-02-22',
-        day: 'Saturday',
-        hour: '2:30 PM',
-        description: 'Quarterly Planning Meeting'),
-  ];
+  MyMeetsSchema? meetings;
+
+  @override
+  void initState() {
+    super.initState();
+      API.post(context, '/my-meets/get').then((Response res) {
+        if (res.hasError) {
+          return;
+        }
+
+        setState(() {
+          meetings = MyMeetsSchema.fromJson(res.data);
+        });
+      });
+  }
+
+  void viewMeetingOnPressed(MeetInfoSchema meeting){
+    Provider.of<AppModel>(context, listen: false).moveToViewMeetingPage(meeting.meetId, meeting.groupId);
+  }
 
   @override
   Widget build(BuildContext context) {
-    meetings.sort((a, b) => a.date.compareTo(b.date));
+    if (meetings == null) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Meetings'),
+        ),
+        body: const Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    meetings!.meets.sort((a, b) => a.meetDate.compareTo(b.meetDate));
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Meetings'),
       ),
       body: ListView.builder(
-        itemCount: meetings.length,
+        itemCount: meetings!.meets.length,
         itemBuilder: (context, index) {
-          final meeting = meetings[index];
+          final meeting = meetings!.meets[index];
           return Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
             child: Container(
@@ -106,111 +64,28 @@ class _MyMeetingsPageState extends State<MyMeetingsPage> {
               ),
               child: ListTile(
                 title: Text(
-                  '${meeting.date}, ${meeting.day}',
+                  '${meeting.groupName} Meeting ${(DateFormat('dd-MM-yyyy').format(meeting.meetDate))}',
                   style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
                 subtitle: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Time: ${meeting.hour}'),
-                    Text('Description: ${meeting.description}'),
+                    Text('Address: ${meeting.city}, ${meeting.street}'),
+                    Text('Time: ${meeting.startTime.hour}:${meeting.startTime.minute.toString().padLeft(2, '0')} - ${meeting.endTime.hour}:${meeting.endTime.minute.toString().padLeft(2, '0')}'),
+                    Text('Duration: ${meeting.duration} minutes'),
                   ],
                 ),
                 trailing: IconButton(
                   icon: const Icon(Icons.arrow_forward),
                   onPressed: () {
-                    // Navigate to meeting page here
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => _MeetingPage(
-                          meeting: meeting,
-                          onMeetingCancelled: () {
-                            // Remove the cancelled meeting from the list
-                            setState(() {
-                              meetings.remove(meeting);
-                            });
-                          },
-                        ),
-                      ),
-                    );
+                    viewMeetingOnPressed(meeting);
                   },
                 ),
               ),
             ),
           );
         },
-      ),
-    );
-  }
-}
-
-class _MeetingPage extends StatelessWidget {
-  final Meeting meeting;
-  final VoidCallback onMeetingCancelled;
-
-  const _MeetingPage({
-    Key? key,
-    required this.meeting,
-    required this.onMeetingCancelled,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Meeting Details'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Date: ${meeting.date}'),
-            Text('Day: ${meeting.day}'),
-            Text('Hour: ${meeting.hour}'),
-            Text('Description: ${meeting.description}'),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Implement cancellation logic here
-                showDialog(
-                  context: context,
-                  builder: (context) => AlertDialog(
-                    title: const Text('Cancel Meeting'),
-                    content: const Text(
-                        'Are you sure you want to cancel this meeting?'),
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context); // Close the dialog
-                        },
-                        child: const Text('No'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          // Implement cancellation logic here
-                          Navigator.pop(context); // Close the dialog
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text('Meeting cancelled'),
-                              duration: Duration(seconds: 2),
-                            ),
-                          );
-                          // Call the callback function to notify the parent page
-                          onMeetingCancelled();
-                        },
-                        child: const Text('Yes, Cancel'),
-                      ),
-                    ],
-                  ),
-                );
-              },
-              child: const Text('Cancel Meeting'),
-            ),
-          ],
-        ),
-      ),
+      )
     );
   }
 }
